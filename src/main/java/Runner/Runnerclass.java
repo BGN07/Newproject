8877cc619646162960299f13b10348f3dd42349e
unknown <nathgopi657@gmail.com>
2025-07-17 09:18:06 +0530

    cucumber framework

---
+++ src/main/java/Runner/Runnerclass.java
@@ -0,0 +1,16 @@
+package Runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+
+@CucumberOptions(features = {"features"},
+                 glue =     {"StepDefinations"},
+                 plugin =    { "pretty","html:target/cucumber-reports",
+    		     "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+                	tags="Smoke",	 
+                  monochrome = true )
+
+public class Runnerclass extends AbstractTestNGCucumberTests{
+	
+	}
